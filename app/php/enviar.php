10a0498bbe168cf1b5feb4b8554d41721c6c019c
Dani <danielpescadormateo@gmail.com>
2015-06-14 21:24:02 +0200

    TOdo ok

--- app/php/enviar.php
+++ app/php/enviar.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Enviar</title>
+<title>Enviar</title>
 </head>
 <body>
-
-
 <?php
 $mensualidad=$_POST['pago'];
-echo "Se procede a la primera mensualidad de: $mensualidad euros";
-echo "<br><a href='../index.html' >Aceptar</a>  <a href='../index.html'>Cancelar</a>";
+echo "Se va a proceder al cobro de la primera factura con un total de $mensualidad euros";
+echo "<br><a href='../index.html' >Aceptar</a> <a href='../index.html'>Cancelar</a>";
 ?>
-
 </body>
 </html>
